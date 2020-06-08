453a1ce7e2a3217f8197db4335001619a3552fd7
Nicholas O'Connor <nicko@nicholass-mbp.local>
2020-06-07 17:05:23 -0700

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends React.Component {
             {
                 title: 'usa',
                 imageUrl: 'https://lh3.googleusercontent.com/proxy/Os-94hWKsUYOomGlgnXH_b-e5u_XdbfOIdrJQJJr5bMEdyAYfDSIHmlSYXw9-Qpm66pral8DsLNxV3CFeE4YOicRvnzcYgtrBgBKQwua8TLmevfh4wjm1P6-OYg8RZsBnh9OXQcTiv9nW1XbS2AASxFEYw',
-                id: 1
+                id: 1,
+                linkUrl: 'usa'
             },
             {
                 title: 'canada',
                 imageUrl: 'https://britishcolumbia.com/images/maps/south-vancouver-island.gif',
-                id: 2   
+                id: 2,
+                linkUrl: 'canada'   
             },
             {
                 title: 'asia',
                 imageUrl: 'https://webmotriz.com.br/wp-content/new-friends-colony-map-i5.gif',
-                id: 3    
+                id: 3,
+                linkUrl: 'asia'    
             },
             {
                 title: 'middle east',
                 imageUrl: 'https://www.orangesmile.com/common/img_city_maps/baghdad-map-2.jpg',
                 size: 'large',
-                id: 4    
+                id: 4,
+                linkUrl: 'middle-east'    
             },
             {
                 title: 'europe',
                 imageUrl: 'https://www.acitymap.com/wp-content/uploads/2015/03/rome-vector-map.jpg',
                 size: 'large',
-                id: 5    
+                id: 5,
+                linkUrl: 'europe'    
             }]
         }
     }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className="directory-menu">
-            {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
             ))}
             </div>
         )
